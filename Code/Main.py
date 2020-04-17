abcfa82f9cd11a1c4c07a2116a98bf9f99342dcf
kovaley <kovaley@hotmail.com>
2020-04-16 20:01:08 -0400

    1

--- Code/Main.py
+++ Code/Main.py
@@ -7,6 +7,11 @@ import ploting as plt
 import time
 import Terme_Source as TS
 import nu_grid as nu_grid
+import simulate 
+import matplotlib.pyplot as plot
+"constants"
+Tcurie=310
+sampling_depth=9
 
 "Dimensions"
 Lr=300e-9
@@ -17,6 +22,7 @@ deltat=0.5e-9;
 deltar=3e-9;
 deltaz=3e-9;
 
+
 Nr=int(np.round(Lr/deltar))
 Nz=int(np.round(Lz/deltaz))
 Nt=int(np.round(duration/deltat))
@@ -24,17 +30,18 @@ Nt=int(np.round(duration/deltat))
 
 P_las=10**np.arange(3)*1e-3   #W  
 resultats=[]
-
+rayonbit=[]
 print(len(P_las))
 
 for n in range(0,len(P_las)):
-    resultats.append(simulate(P_las[n],deltaz,deltar,deltat,Lr,Lz,duration))
-
+    resultats.append(simulate.simulate(P_las[n],deltaz,deltar,deltat,Lr,Lz,duration))
+    rayonbit.append(deltar*np.sum(resultats[n][:,sampling_depth,:]>Tcurie,axis=0))
 
 "Plotting et animation"
+plot.plot(deltat*np.arange(0,Nt),rayonbit[0])
 
-plt.animate(resultats[0])
-plt.animate(resultats[1])
+#plt.animate(resultats[0])
+#plt.animate(resultats[1])
 # plt.animate(source,Nt)
 
 
