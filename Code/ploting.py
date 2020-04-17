0ff0295f3cccb356d8586ea71aa3a18c1c45d600
spartan934 <60269957+spartan934@users.noreply.github.com>
2020-04-16 21:46:02 -0400

    anim ralenti

--- Code/ploting.py
+++ Code/ploting.py
@@ -24,7 +24,7 @@ def animate(maille):
         
     plt.gca().invert_yaxis()
     plt.colorbar(sm(norm=norm))
-    im_ani = animation.ArtistAnimation(fig1, ims, interval=50, repeat_delay=1000,
+    im_ani = animation.ArtistAnimation(fig1, ims, interval=150, repeat_delay=1000,
                                    blit=True)
     
 # To save this second animation with some metadata, use the following command:
